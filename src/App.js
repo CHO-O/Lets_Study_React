8c1a870fb801dc5aed44e61ec2ce5b43d0fc809f
CHO-O <327cjm@gmail.com>
2023-02-09 15:33:59 +0900

    22. Context API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useReducer, useRef, useMemo, useCallback } from "react";
 import UserList from "./UserList";
 import CreateUser from "./CreateUser";
-import useInputs from "./hooks/useInputs";
 
 function countActiveUsers(users) {
   console.log('활성 사용자 수를 세는중...');
@@ -60,48 +59,24 @@ function reducer(state, action){
 }
 //1. reducer함수의 틀 만듬
 
+export const UserDispatch = React.createContext(null);
+//const UserDispatch = createContext()함수는 Context를 만드는 함수. 
+//파라미터에는 Context의 기본값을 설정 가능
+//export ~ React.는 내보내주는 작업
+//내보냈기 때문에 나중에 사용하고 싶을 때 import { UserDispatch } from './App';으로 불러올 수 있음
+
 function App(){
-  const [{ username, email }, onChange, reset] = useInputs({
-    username: '',
-    email: ''
-  });
-  //기존 코드에 존재하는 inputs들, onchange를 전부 없앰
-  //새로운 항목을 추가할 때 input값을 초기화하기 위한 reset() 호출
+
+  //Context로 돌릴거니까 useinputs관련 싹다 지우기
 
   const [state, dispatch] = useReducer(reducer, initialState);
-  const nextId = useRef(4);
+ 
+  //useRef 이용한 nextId도 Createuser로 넘길거니까 지워버리기
 
   const { users } = state;
 
-  const onCreate = useCallback(() => {
-    dispatch({
-      type: 'CREATE_USER',
-      user: {
-        id: nextId.current,
-        username,
-        email
-      }
-    });
-    reset(); //초기화. onCreate는 input값을 사용하니까...
-    nextId.current += 1;
-  }, [username, email, reset]);
-  //4. onCreate 구현
-
-  const onToggle = useCallback(id => {
-    dispatch({
-      type: 'TOGGLE_USER',
-      id
-    });
-  }, []);
-  //5. onToggle 구현
-
-  const onRemove = useCallback(id =>{ 
-    dispatch({
-      type: 'REMOVE_USER',
-      id
-    });
-  }, []);
-  //6. onRemove 구현
+  //Context를 구현했으니 onToggle과 onRemove를 지웠음. UserList에서도 지우러 갈거임
+  //onCreat도 지워버렸음. 이제 CreateUser.js로 갑시다
 
   const count = useMemo(() => countActiveUsers(users), [users]);
   //7. 활성 사용자 수 구하기 구현
@@ -110,18 +85,21 @@ function App(){
   //   도저히 이유를 모르겠음!!!!!!!!!!!!!!!! 
 
   return (
-    <>
-      <CreateUser 
-        username={username}
-        email={email}
-        onChange={onChange}
-        onCreate={onCreate}
-      />
-      <UserList users={users} onToggle={onToggle} onRemove={onRemove} /> 
-      {/* UserList 컴포넌트에게 함수를 전달해주는 것 잊지 말기 */}
+    <UserDispatch.Provider value={dispatch}> 
+      <CreateUser /> {/* context쓰러 넘어갑시다 */}
+      <UserList users={users} /> {/* UserList 컴포넌트에게 함수를 전달해주기 */}
       <div>활성사용자 수 : {count}</div>
-    </>
+    </UserDispatch.Provider>
   );
 }
+
+//Context안에는 Provider라는 컴포넌트가 들어있음
+//이 컴포넌트를 통해 Context의 값을 정할 수 있으며, 사용할 때 value값을 설정해주면 된다
+//지금 공유하길 원하는 데이터의 초깃값은 dispatch가 되는 것
+//Provider에 의해 감싸진 컴포넌트 중 어디서든 Context의 값을 다른 곳에서 바로 조쇠해서 사용할 수 있음
+
+//+CreateUser 에게는 아무 props 도 전달하지 마세요.
+//CreateUser 컴포넌트 내부에서 useInputs 를 사용하세요.
+//useRef 를 사용한 nextId 값을 CreateUser 에서 관리하세요.
 
 export default App;
--- src/UserList.js
+++ src/UserList.js
@@ -1,6 +1,9 @@
-import React, { useEffect } from "react";
+import React, { useContext } from "react"; // useContext라는 Hook으로 만든 context를 조회
+import { UserDispatch } from "./App"; //아까 App에서 내보냈던거 가져옴
 
-const User = React.memo(function User({ user , onRemove , onToggle }) {
+const User = React.memo(function User({ user }) {
+    const dispatch = useContext(UserDispatch); //가져왔따
+
     return(
         <div>
             <b 
@@ -8,24 +11,26 @@ const User = React.memo(function User({ user , onRemove , onToggle }) {
                     cursor: 'pointer',
                     color: user.active ? 'green' : 'black'
                 }}
-                onClick={() => onToggle(user.id)} //클릭시, onToggle에 id를 넣어서 호출함
+                onClick={() => {
+                    dispatch({ type: 'TOGGLE_USER', id: user.id }); //onToggle이었던것
+                }} 
             > {user.username}</b>
             &nbsp; {/*공백문자*/}
             <span>({user.email})</span>
-            <button onClick={() => onRemove(user.id)}>삭제</button>
+            <button onClick={() => {
+                dispatch({ type: 'REMOVE_USER', id: user.id }); //onRemove이었던것
+            }}>삭제</button>
         </div>
     );
 });
 
-function UserList({ users, onRemove, onToggle}) { //UserList 컴포넌트가 받아온 함수들
+function UserList({ users }) { //UserList 컴포넌트가 받아온 함수들
     return (
         <div>
             {users.map(user => (
                 <User 
                     user={user} 
                     key={user.id} 
-                    onRemove={onRemove}
-                    onToggle={onToggle} // User에게 전달하였음
                 />
             ))}
         </div>
@@ -33,3 +38,7 @@ function UserList({ users, onRemove, onToggle}) { //UserList 컴포넌트가 받
 }
 
 export default React.memo(UserList);
+
+//현재 UserList 컴포넌트는 App 컴포넌트에서 구현된 onToggle, onRemove를 User컴포넌트에게 전달하는
+//중간다리 역할만 하고 있음
+//Context API와 Dispatch를 이용하면 이 복잡한 구조를 해결할 수 있음
